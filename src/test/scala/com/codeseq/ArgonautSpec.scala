1480cd957c45dc9e9f10c0b9f2e552d9dd57f42c
Derek Wyatt <derek@derekwyatt.org>
2014-01-14 20:22:42 -0500

    Somehow the file got messed up

--- src/test/scala/com/codeseq/ArgonautSpec.scala
+++ src/test/scala/com/codeseq/ArgonautSpec.scala
@@ -1,3 +1,41 @@
+package com.codeseq
+
+import org.scalatest.{WordSpec, Matchers}
+
+class ArgonautSpec extends WordSpec with Matchers {
+  val jsonStr = loadJson()
+
+  def go(): Unit = 
+    for (_ <- 1 to 1000) {
+      Argonaut.parseToString(jsonStr)
+    }
+
+  "Argonaut" should { //{1
+    "parse it 1,000 times (01)" in { //{2
+      go()
+    } //}2
+    "parse it 1,000 times (02)" in { //{2
+      go()
+    } //}2
+    "parse it 1,000 times (03)" in { //{2
+      go()
+    } //}2
+    "parse it 1,000 times (04)" in { //{2
+      go()
+    } //}2
+    "parse it 1,000 times (05)" in { //{2
+      go()
+    } //}2
+    "parse it 1,000 times (06)" in { //{2
+      go()
+    } //}2
+    "parse it 1,000 times (07)" in { //{2
+      go()
+    } //}2
+    "parse it 1,000 times (08)" in { //{2
+      go()
+    } //}2
+    "parse it 1,000 times (09)" in { //{2
       go()
     } //}2
     "parse it 1,000 times (10)" in { //{2
@@ -6,3 +44,4 @@
   } //}1
 }
 // vim:fdl=1:
+
