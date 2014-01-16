c927e945b5e4d11ca00784979cb88dd94da56b99
Andre Silva <andre.beat@gmail.com>
2014-01-16 00:57:07 +0000

    refactor specs

--- src/main/scala/com/codeseq/Parboiled2.scala
+++ src/main/scala/com/codeseq/Parboiled2.scala
@@ -1,7 +1,7 @@
 package com.codeseq
 
-import org.parboiled2.examples.JsonParser
+import org.parboiled2.examples.{JsonParser => Parboiled2JsonParser}
 
-object Parboiled2 {
-  def parseToString(jsonStr: String): String = new JsonParser(jsonStr).Json.run().get.compactPrint
+object Parboiled2 extends JsonParser {
+  def parseToString(jsonStr: String): String = new Parboiled2JsonParser(jsonStr).Json.run().get.compactPrint
 }
